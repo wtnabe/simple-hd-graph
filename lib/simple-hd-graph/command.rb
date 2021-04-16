9d3c0fabe813fa889325178be7f1f0b8e8eb54bf
wtnabe <wtnabe@gmail.com>
2021-04-17 08:31:30 +0900

    modify SimpleHdGraph::Command interface

--- lib/simple-hd-graph/command.rb
+++ lib/simple-hd-graph/command.rb
@@ -1,21 +1,37 @@
 require 'optparse'
 
 module SimpleHdGraph
+  class FileNotExist < StandardError; end
+
   class Command
-    def initialize(argv)
-      @argv = argv.dup
+    #
+    # @param argv [Array]
+    #
+    def run(argv)
+      parse(argv)
+      if @file
+        start
+      end
     end
 
-    def run
-      opts.parse(@argv)
-      if @file
-        nodes = Parser.new.parse(Reader.new.read_file(@file))
-        renderer = Renderer::PlantUML::Context.new
+    #
+    # @param argv [Array]
+    #
+    def parse(argv)
+      opts.parse(argv)
+    end
 
-        puts nodes.map { |node|
-          renderer.render(node)
-        }.join
-      end
+    #
+    # @param parser [Parser]
+    # @param reader [Reader]
+    # @param renderer [Renderer]
+    #
+    def start(parser: Parser.new, reader: Reader.new, renderer: Renderer::PlantUML::Context.new)
+      nodes = parser.parse(reader.read_file(@file))
+
+      puts nodes.map { |node|
+        renderer.render(node)
+      }.join
     end
 
     #
@@ -25,7 +41,11 @@ module SimpleHdGraph
     def opts
       OptionParser.new do |opt|
         opt.on('-f FILE', '--file', 'filename') { |value|
-          @file = value
+          if File.exist?(value) && File.file?(value)
+            @file = value
+          else
+            raise FileNotExist, value
+          end
         }
       end
     end
---
+++ spec/command_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe SimpleHdGraph::Command do
+  def command
+    @command ||= SimpleHdGraph::Command.new
+  end
+
+  describe '#parse' do
+    describe 'file' do
+      describe 'exists' do
+        it {
+          assert {
+            command.parse(['-f', File.join(__dir__, 'support/example_simple.yaml')])
+          }
+        }
+      end
+
+      describe 'not exist' do
+        it {
+          assert_raises SimpleHdGraph::FileNotExist do
+            command.parse(['-f', 'notexist'])
+          end
+        }
+      end
+    end
+  end
+end
